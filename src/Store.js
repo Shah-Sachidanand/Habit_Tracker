04dd31ad9cf32d0af8c0f6fcabd727c7dda4923f
Sachidanand Shah <s.shahmlg@gmail.com>
2022-07-25 16:50:23 +0530

    antd Lib Added

---
+++ src/Store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+import reducer from '../reducers';
+
+//to store store data in localStorage
+const saveLocal = (state) => {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem('goal', serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+//to get data from localStorage
+const loadLocal = () => {
+  try {
+    const serialisedState = localStorage.getItem('goal');
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+};
+
+//return store with middleware
+let store;
+export function configureStore() {
+  store = createStore(reducer, loadLocal());
+  store.subscribe(() => saveLocal(store.getState()));
+
+  return store;
+}
