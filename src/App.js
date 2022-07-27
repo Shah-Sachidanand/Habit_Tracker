69c7ba7c31eec991e97af0c0935ecdba812f0343
Sachidanand Shah <s.shahmlg@gmail.com>
2022-07-27 14:34:42 +0530

    app.js Modified

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Main from './components/Main';
 
 function App() {
 
+  const style = {
+    color:'Gold',
+    margin:'200px',
+    height: '50px',
+    width: '490px',
+    marginLeft:'33.33%',
+    // marginTop:'33.33%'
+  }
   //render layout- header-navbar content-main and tracker and footer
   return (
     <div className="App">
     <Navbar />
+    <h1 style={style}> Welcome To Habit Tracker</h1>
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/progress' element={<Tracker />} />
